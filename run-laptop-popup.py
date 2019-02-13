2212c2e2cf2c9c1f525763663653b4f7bd753dba
Larry Boltovskoi <larry.bolt@gmail.com>
2019-02-13 21:44:02 +0100

    Only show frame with annotations when detected face

--- run-laptop-popup.py
+++ run-laptop-popup.py
@@ -1,3 +1,6 @@
+import numpy
+
+
 from cameras.laptopcam import stream_video
 #from detectors.simple import detect_face
 from detectors.rollingcaro import detect_face
@@ -8,9 +11,11 @@ cascadePath = './models/opencv/haarcascade_frontalface_default.xml'
 
 
 def every_frame(frame):
-    person_detected, frame_with_face = detect_face(frame, cascadePath)
+    person_detected, frame_with_face = detect_face(numpy.copy(frame), cascadePath)
     if person_detected:
         show_frame(frame_with_face)
+    else:
+        show_frame(frame)
 
 
 stream_video(every_frame)
