62ac54f22fc1b990f9e469508f0c2829c9d3ffcd
Larry Boltovskoi <larry.bolt@gmail.com>
2019-02-18 14:53:50 +0100

    Resize image

--- cameras/laptop_cam.py
+++ cameras/laptop_cam.py
@@ -7,6 +7,7 @@ def stream_video(result_callback: Callable):
     video_capture = cv2.VideoCapture(0)
     while True:
         ret, frame = video_capture.read()
-        result_callback(frame)
+        img = cv2.resize(frame, None, fx=0.5, fy=0.5)
+        result_callback(img)
     video_capture.release()
     cv2.destroyAllWindows()
