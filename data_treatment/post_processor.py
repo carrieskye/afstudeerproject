9361eaf10415c688af36b1f33cbb45ba499c8780
Larry Boltovskoi <larry.bolt@gmail.com>
2019-02-21 15:49:30 +0100

    Use median for age instead of average 🤩

--- data_treatment/post_processor.py
+++ data_treatment/post_processor.py
@@ -6,8 +6,10 @@ import numpy as np
 
 
 def get_overall_classification(classifications):
-    # get average age over all ages classifications
-    average_age = int(reduce(np.add, (c.age for c in classifications)) / len(classifications))
+    # if we want to use average instead:
+    # average_age = int(reduce(np.add, (c.age for c in classifications)) / len(classifications))
+    # get median age over all ages classifications
+    median_age = int(np.median(list(c.age for c in classifications)))
 
     # get most common gender label
     most_common_gender = Counter(c.gender for c in classifications).most_common(1)[0][0]
@@ -18,4 +20,4 @@ def get_overall_classification(classifications):
     last_position = classifications[-1].position
     last_emotion = classifications[-1].emotion
 
-    return Classification(last_timestamp, last_name, last_position, most_common_gender, last_emotion, average_age)
+    return Classification(last_timestamp, last_name, last_position, most_common_gender, last_emotion, median_age)
